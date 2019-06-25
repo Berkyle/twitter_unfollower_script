5058198c1db6d6b64324f2210f7c4722df2e639e
Berkyle <34729254+Berkyle@users.noreply.github.com>
2019-06-25 16:47:33 -0500

    added utility methods.

--- utils/utils.js
+++ utils/utils.js
@@ -1,19 +1,56 @@
-const path = require('path');
+// Create HTML string of menu-like links
+const createDirMenu = (route, accounts, all = true) => {
+    let jsx = `<br><br>`;
 
+    if(all) 
+        jsx += `<a href="/${route}/all">all</a><br>`;
+
+    for(let acc of accounts)
+        jsx += `<a href="/${route}/${acc[0]}">${acc[0]}</a><br>`;
+
+    return jsx;
+}
+
+// Generate filename to store user's follower data
+const path = require('path');
+const fileName = (id, n) =>
+    path.join(__dirname, '../lib/data', `${id}_${n}.json`);
+
+// Retrieve only the desired account
+const findAccount = (uname, accounts) => 
+    accounts.find(account => account[0] === uname);
+
+// Concatentate arrays together
+const flatten = arr =>
+    arr.reduce((acc, val) => acc.concat(val), []);
+
+// Break an array of users into segments of 100 users
+const segment100 = followers => {
+    let in100s = [];
+    const arrayMax = followers.length - 1;
+    for(let i=0; i<arrayMax; i+=100)
+        in100s[i/100] = followers.slice(i, i + Math.min(arrayMax-i, 99));
+
+    return in100s;
+}
+
+// Sort an array of users by their follow count (summarized first)
+const sortByFollowers = summarized =>
+    summarized.sort((a, b) => b.followers - a.followers);
+
+// Summarize each value in an array of user objects
 const summarize = data => data.map(acc => ({
         user: `${acc.name} (@${acc.screen_name}, id ${acc.id_str})`,
         followers: acc.followers_count,
         following: acc.friends_count
     }));
 
-const sortByFollowers = summarized => 
-    summarized.sort((a, b) => b.followers - a.followers);
-
-const fileName = (id, n) => 
-    path.join(__dirname, '../lib/data', `${id}_${n}.json`);
-
 module.exports = {
-    summarize,
+    createDirMenu,
+    fileName,
+    findAccount,
+    flatten,
+    segment100,
     sortByFollowers,
-    fileName
-};  
+    summarize
+};
