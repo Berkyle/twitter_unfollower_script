c58e79aaa032a752801d54f65781489ce5668e95
Berkyle <34729254+Berkyle@users.noreply.github.com>
2019-06-24 21:18:01 -0500

    sorted features into routes. added business route

--- server.js
+++ server.js
@@ -1,21 +1,35 @@
-const express       = require('express');
-const fs            = require('fs');
+const express   = require('express');
+const fs        = require('fs');
 
-const queryTwitter  = require('./lib/petty');
+const traitors  = require('./lib/petty');
+const business  = require('./lib/business');
 
 let app = express();
 const port = 3000;
 
-app.get('/', (_, res) => {
-    fs.readFile('config/accounts.json', (err, acntStrng) => {
-        const accounts = JSON.parse(acntStrng).accounts;
+let accounts = [];
 
-        queryTwitter(accounts).then((data) => {
-            res.send(data);
-        }).catch((error) => {
-            res.send(error);
-        });
+app.get('/', (_, res) => {
+    const jsx1 = `<a href="/traitors">traitors</a><br>`;
+    const jsx2 = `<a href="/business">business</a>`;
+
+    fs.readFile('config/accounts.json', (err, acntStrng) => {
+        accounts = JSON.parse(acntStrng).accounts;
+
+        res.send(jsx1+jsx2);
     });
+});
+
+app.get('/traitors', (_, res) => {
+    traitors(accounts)
+        .then((data) => res.send(data))
+        .catch((error) => res.send(error));
+});
+
+app.get('/business', (_, res) => {
+    business(accounts)
+        .then((data) => res.send(data[0]))
+        .catch((error) => res.send(error));
 });
 
 app.listen(port, () => {
