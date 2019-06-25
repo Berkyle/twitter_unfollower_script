11c4e3828ba35dde4ae227606877e509fc081aa7
Berkyle <34729254+Berkyle@users.noreply.github.com>
2019-06-24 21:13:53 -0500

    added sortByFollowers functionality

--- utils/utils.js
+++ utils/utils.js
@@ -1,15 +1,19 @@
 const path = require('path');
 
-const summarize = data => data
-    .map(acc => ({
+const summarize = data => data.map(acc => ({
         user: `${acc.name} (@${acc.screen_name}, id ${acc.id_str})`,
         followers: acc.followers_count,
         following: acc.friends_count
     }));
 
-const fileName = (id, n) => path.join(__dirname, '../lib/data', `${id}_${n}.json`);
+const sortByFollowers = summarized => 
+    summarized.sort((a, b) => b.followers - a.followers);
+
+const fileName = (id, n) => 
+    path.join(__dirname, '../lib/data', `${id}_${n}.json`);
 
 module.exports = {
     summarize,
+    sortByFollowers,
     fileName
-};
+};  
